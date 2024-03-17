c839a32521d4aa2ee534212cf0f1fe9a10dd3cb6
Daniel Agbeni <99095841+DanielAgbeni@users.noreply.github.com>
2024-03-17 19:12:57 +0100

    Started Store Project

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<div className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary drop-shadow-xl'>
+			{/* Desktop view */}
+			<div className=' flex items-center justify-between'>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			{/* Mobile view */}
+			<div></div>
+		</div>
+	);
+};
+
+export default Navbar;
